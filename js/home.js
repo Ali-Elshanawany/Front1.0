33f2015c536d9c2975905a0cc6946fd0cbb791bb
Rawan-Labeb <rawan.w.labeb@gmail.com>
2024-12-28 23:14:11 +0200

    test

--- js/home.js
+++ js/home.js
@@ -1,7 +1,6 @@
 import { data, loadDataFromLocalStorage, saveDataInLocalStorage, SetUserById ,getCurrentUser, isAuthorized}
     from './Data.js';
 
-isAuthorized();
 // Add to Cart Function 
 export function addToCart(productID) {
     let product = data.Products.find((p) => p._id === productID);
@@ -95,6 +94,7 @@ export function addToCart(productID) {
 function initializePage() {
     loadDataFromLocalStorage();
     saveDataInLocalStorage();
+
 
     // Sticky Navbar
     const firstNavbarHeight = $('.navbar-main').outerHeight();
@@ -390,5 +390,6 @@ setupLoginButton();
 }
 $(document).ready(function () {
     initializePage();
+
     console.log("Page initialized.");
 });
